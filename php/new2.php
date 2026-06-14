8a2b2a094492d232aac9698803fe1ff9225fcbc1
xxxb-g <xxxbGamer@proton.me>
2026-06-14 17:36:55 +0200

    E-Mail Benachrichtigung bei neuer Meldung
    Fügt hinzu, dass eine E-Mail gesendet wird, wenn ein neuer Beitrag eingereicht wurde.

--- php/new2.php
+++ php/new2.php
@@ -17,13 +17,23 @@ $position_text = $data['position_text']!='' ? "'".$data['position_text']."'" : "
 $query=$mysql->query("INSERT INTO stellen (lat, lng, position_text, Titel, Problem, Loesung, Kategorie) VALUES ($lat, $lng, $position_text, $Titel, $Problem, $Loesung, 'Eingereicht')");
 $id = $mysql->insert_id;
 $mysql->query("INSERT INTO mails (fk_stelle_id, mail) VALUES ($id, $Mail);");
+// Benachrichtigung an Admin senden
+$admin_email = $sender_mail_address;
+$subject = "Neue Meldung auf Pirna800 Karte (ID $id)";
+$message = "Eine neue Meldung wurde eingereicht.\n\n";
+$message .= "ID: $id\n";
+$message .= "Titel: ".($data['Titel'] ?? '')."\n";
+$message .= "Problem: ".($data['Problem'] ?? '')."\n";
+$message .= "Position: ".($data['position_text'] ?? '')."\n";
+$header = 'From: '.$sender_mail_address.' '. "\r\n" .'Reply-To: '.$sender_mail_address;
+@mail($admin_email, $subject, $message, $header);
 if($data['BildURI']!="") {
-  $DataUriRaw = $data['BildURI'];
-  $DataUriSep = explode(',', $DataUriRaw);
-  $DataImage = base64_decode($DataUriSep[1]);
-  $filename = $id."-".date("YmdHis").".jpg";
-  $filepath = $images_folder.'/'.$filename;
-  file_put_contents($filepath,$DataImage);
-  $mysql->query("UPDATE stellen SET Bild='$filename' WHERE id=$id;");
+    $DataUriRaw = $data['BildURI'];
+    $DataUriSep = explode(',', $DataUriRaw);
+    $DataImage = base64_decode($DataUriSep[1]);
+    $filename = "B".$id."-".date("YmdHis").".jpg";
+    $filepath = $images_folder.'/'.$filename;
+    file_put_contents($filepath,$DataImage);
+    $mysql->query("UPDATE stellen SET Bild='$filename' WHERE id=$id;");
 }
 ?>
